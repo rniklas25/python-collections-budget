e64a98a17128ba735bbf7d854245ab7b9864cce6
Ryan Niklas <rniklas25@gmail.com>
2022-08-24 09:08:26 -0400

    Budget

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -25,7 +25,7 @@ class BudgetList:
 def main():
     myBudgetList = BudgetList(1200)
     expenses = Expense.Expenses()
-    expenses.read_expenses('data / spending_data.csv')
+    expenses.read_expenses('data/spending_data.csv')
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
 
