980b4b6ed4a412eee93b3f8d952711b530ac56ff
Ryan Niklas <rniklas25@gmail.com>
2022-08-24 08:46:27 -0400

    Budget

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,5 +1,16 @@
 
 
 class BudgetList:
-    pass
+
+    def __init__(self, budget):
+        self.budget = budget
+        self.sum_expenses = 0
+        self.expenses = []
+        self.sum_overages = 0
+        self.overages = []
+
+    def append(self, item):
+        if (self.sum_expenses + item < self.budget):
+            self.expenses.append(item)
+            item + self.sum_expenses
 
